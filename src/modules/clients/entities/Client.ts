38d542b561dfb841691c7bef5066046e7fb7ca48
Danillo Rodrigues <danillorodriguesabreu@gmail.com>
2023-03-03 16:30:32 -0300

    create-migration-table-client (#11)

---
+++ src/modules/clients/entities/Client.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidV4 } from "uuid";
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
+
+@Entity("Clients")
+class Client {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  fone: string;
+
+  @Column()
+  cpf: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Client };

ce2a9bdd1bf052365775bea97f1d469acfdf469a
danillobr <danillorodriguesabreu@gmail.com>
2023-03-03 16:27:46 -0300

    create-migration-table-client

---
+++ src/shared/infra/typeorm/migrations/1677869920156-CreateClient.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateClient1677869920156 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "clients",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "fone",
+            type: "varchar",
+          },
+          {
+            name: "cpf",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("clients");
+  }
+}

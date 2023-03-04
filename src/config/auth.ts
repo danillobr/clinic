fd19ef6be64d5ea8f342996df6910a1169886b0b
Danillo Rodrigues <danillorodriguesabreu@gmail.com>
2023-03-04 11:07:05 -0300

    Revert "Issue 1609788078 create table and migration attendant feature (#20)" (#21)
    This reverts commit f74f249a7d5184d9e2180fb2699e7e1ec569fdb5.

--- src/config/auth.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  secret_token: "14c208b3c86ff07280ca891ddc370608",
-};
--- src/shared/container/index.ts
+++
@@ -1,9 +0,0 @@
-import { container } from "tsyringe";
-
-import { ClientsRepository } from "@modules/clients/infra/typeorm/repositories/ClientsRepository";
-import { IClientsRepository } from "@modules/clients/repositories/IClientsRepository";
-
-container.registerSingleton<IClientsRepository>(
-  "ClientsRepository",
-  ClientsRepository
-);

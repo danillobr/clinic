6b1f6515082d8ca280e360d019a0332b161a7d30
Danillo Rodrigues <danillorodriguesabreu@gmail.com>
2023-03-04 11:35:34 -0300

    attendant-bug (#24)

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,6 @@
 import { container } from "tsyringe";
 
-import { ClientsRepository } from "@modules/clients/infra/typeorm/repositories/ClientsRepository";
+import { ClientsRepository } from "@modules/attendant/infra/typeorm/repositories/ClientsRepository";
 import { IClientsRepository } from "@modules/clients/repositories/IClientsRepository";
 
 container.registerSingleton<IClientsRepository>(
